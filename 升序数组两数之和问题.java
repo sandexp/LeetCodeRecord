e161f6e9273a832b04db1f793e1bfc5825d915ca
XieYunfei0921 <XieYunfei0921@126.com>
2020-12-18 14:58:59 +0800

    Create 升序数组两数之和问题.java

---
+++ 升序数组两数之和问题.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        if(numbers==null)
+            return null;
+        int p1=0,p2=numbers.length-1;
+        while(p1<p2){
+            if(numbers[p1]+numbers[p2]==target)
+                return new int[]{p1+1,p2+1};
+
+            if(numbers[p1]+numbers[p2]>target){
+                p2--;
+            }else{
+                p1++;
+            }
+        }
+        return null;
+    }
+}
