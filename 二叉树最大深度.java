145858f37e076ad8ae780c1438c19d911294c000
XieYunfei0921 <XieYunfei0921@126.com>
2021-02-02 23:48:20 +0800

    Update 二叉树最大深度.java

---
+++ 二叉树最大深度.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    public int dfs(TreeNode root){
+        if(root==null)
+            return 0;
+        return Math.max(dfs(root.left)+1,dfs(root.right)+1);
+    }
+
+    public int maxDepth(TreeNode root) {
+        return dfs(root);
+    }
+}
