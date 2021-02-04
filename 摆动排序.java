899a29bb26597101e391537784f46e39ba325a17
XieYunfei0921 <XieYunfei0921@126.com>
2021-02-04 18:12:51 +0800

    Create 摆动排序.java

---
+++ 摆动排序.java
@@ -0,0 +1,11 @@
+class Solution {
+    public void wiggleSort(int[] nums) {
+
+       Arrays.sort(nums);
+       for(int i=1;i<nums.length-1;i+=2){
+           int tmp=nums[i];
+           nums[i]=nums[i+1];
+           nums[i+1]=tmp;
+       }
+    }
+}
